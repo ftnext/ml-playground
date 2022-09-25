71765c675a02f40e1aca75a9a9f7b4299a6be436
ftnext <takuyafjp+develop@gmail.com>
2022-09-25 19:11:18 +0900

    [feat] Example 2-way holdout evaluation

---
+++ how_to_evaluate/model_evaluation/holdout_2way.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+if __name__ == "__main__":
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, shuffle=True, stratify=y, random_state=42
+    )
+    print(f"train: {len(y_train)}, test: {len(y_test)}")
+
+    classifier = DecisionTreeClassifier(random_state=1)
+    classifier.fit(X_train, y_train)
+    score = classifier.score(X_test, y_test)
+    print(f"ACC: {score * 100:.2f}")
+
+    # TODO あとは全データで訓練し、モデルを保存する
