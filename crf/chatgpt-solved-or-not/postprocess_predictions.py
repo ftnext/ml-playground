dd949190fcdf2a51f7ac61f4186612661ffb6caa
ftnext <takuyafjp+develop@gmail.com>
2023-07-22 17:19:02 +0900

    [refactor] Reuse NER tag id map

--- crf/chatgpt-solved-or-not/postprocess_predictions.py
+++ crf/chatgpt-solved-or-not/postprocess_predictions.py
@@ -5,21 +5,8 @@ from pathlib import Path
 
 import jsonlines
 
+from constants import ner_tags
 from custom_types import Prediction
-
-# ref: https://huggingface.co/datasets/conll2003
-ner_tags = {
-    "O": 0,
-    "B-PER": 1,
-    "I-PER": 2,
-    "B-ORG": 3,
-    "I-ORG": 4,
-    "B-LOC": 5,
-    "I-LOC": 6,
-    "B-MISC": 7,
-    "I-MISC": 8,
-}
-index2tag = {v: k for k, v in ner_tags.items()}
 
 verbose2short = {
     "Person": "PER",
