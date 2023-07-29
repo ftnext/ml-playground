4714b97e37665a351de55188c7548fdb33ea73ee
ftnext <takuyafjp+develop@gmail.com>
2023-07-29 17:29:44 +0900

    [bugfix] Fix in processing train and val data

--- crf/ousia-llm-book/snippets/iob2_labels.py
+++ crf/ousia-llm-book/snippets/iob2_labels.py
@@ -1,4 +1,6 @@
 from collections.abc import Iterable
+from difflib import Differ
+from pprint import pformat
 
 from spacy_alignments import get_alignments
 from transformers import AutoTokenizer, PreTrainedTokenizer
@@ -20,12 +22,23 @@ def tokenize(text: str, tokenizer: PreTrainedTokenizer) -> list[str]:
 def get_char_to_token_alignments(
     text: str, tokens: list[str]
 ) -> list[list[int]]:
-    assert text == "".join(t.lstrip("#") for t in tokens[1:-1])
+    # [UNK]が入ってくると検証できない
+    if "[UNK]" not in set(tokens):
+        text_without_space = text.replace(" ", "")
+        joined_tokens = "".join(t.removeprefix("##") for t in tokens[1:-1])
+        assert text_without_space == joined_tokens, pformat(
+            diff(text_without_space, joined_tokens)
+        )
 
     characters = list(text)
     # [[1], [1], [1], [2], [2]] のように、何文字目が何番目のトークンかを表す
     char_to_token_indices, _ = get_alignments(characters, tokens)
     return char_to_token_indices
+
+
+def diff(first: str, second: str) -> list[str]:
+    d = Differ()
+    return list(d.compare(first, second))
 
 
 def output_labels(
@@ -40,12 +53,24 @@ def output_labels(
     ... ]
     >>> output_labels(text, tokens, entities)
     ['-', 'B-人名', 'I-人名', 'I-人名', 'O', 'B-地名', 'I-地名', 'I-地名', 'I-地名', 'O', '-']
+
+    >>> entities = [{"name": "矢吹怗一", "span": [0, 4], "type": "人名"}]
+    >>> output_labels("矢吹怗一監督", ["[CLS]", "矢", "##吹", "[UNK]", "監督", "[SEP]"], entities)
+    ['-', 'O', 'O', 'O', 'O', '-']
+    >>> entities = [{"name": "ああいいうう", "span": [0, 6], "type": "法人名"}]
+    >>> output_labels("ああいいうう", ["[CLS]", "ああ", "[UNK]", "うう", "[SEP]"], entities)
+    ['-', 'B-法人名', 'I-法人名', 'I-法人名', '-']
     """
     char_to_token_indices = get_char_to_token_alignments(text, tokens)
 
     labels = ["O"] * len(tokens)
     for entity in entities:
         entity_span, entity_type = entity["span"], entity["type"]
+        start_token_indices = char_to_token_indices[entity_span[0]]
+        end_token_indices = char_to_token_indices[entity_span[1] - 1]
+        # "[UNK]"があるとき、リストが空（start_token_indices[0]がIndexError）
+        if len(start_token_indices) == 0 or len(end_token_indices) == 0:
+            continue
         start_token_id: int = char_to_token_indices[entity_span[0]][0]
         end_token_id: int = char_to_token_indices[entity_span[1] - 1][0]
 
