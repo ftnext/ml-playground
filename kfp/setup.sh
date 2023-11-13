451a40cc8c2c52399627596d71b25ae442d40c0c
ftnext <takuyafjp+develop@gmail.com>
2023-11-13 23:55:35 +0900

    [chore] Kubeflow Pipelines setup script

---
+++ kfp/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
+kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic-pns?ref=$PIPELINE_VERSION"
