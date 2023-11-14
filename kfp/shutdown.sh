5cbfe5a25850cdc043d9720a519ebdbb14ebccba
ftnext <takuyafjp+develop@gmail.com>
2023-11-14 21:40:29 +0900

    chore: Clean up resources

---
+++ kfp/shutdown.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+kubectl delete -k "github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic-pns?ref=$PIPELINE_VERSION"
+kubectl delete -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
