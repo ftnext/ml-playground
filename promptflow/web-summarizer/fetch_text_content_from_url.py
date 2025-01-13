00b7f0419f0709d0fdfc02bd491ce01b7db8726f
ftnext <takuyafjp+develop@gmail.com>
2025-01-13 13:32:59 +0900

    [feat] Summarize given url content

--- promptflow/web-summarizer/fetch_text_content_from_url.py
+++ promptflow/web-summarizer/fetch_text_content_from_url.py
@@ -1,6 +1,26 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "httpx",
+#     "MainContentExtractor",
+#     "promptflow",
+# ]
+# ///
+import httpx
+from main_content_extractor import MainContentExtractor
 from promptflow.core import tool
 
 
 @tool
 def fetch_text_content_from_url(url: str) -> str:
-    return """<ここに決め打ちのHTML>"""
+    response = httpx.get(url)
+    response.raise_for_status()
+
+    response.encoding = "utf-8"
+    return MainContentExtractor.extract(response.text)
+
+
+if __name__ == "__main__":
+    import sys
+
+    print(fetch_text_content_from_url(sys.argv[1]))
